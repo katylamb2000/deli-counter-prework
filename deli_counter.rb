413eaa4bed9e4f3012de14056c72df0c64528167
katy@karstaway.com <katy@karstaway.com>
2018-08-25 15:06:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (katz_deli)
   if katz_deli.length ==0
     puts "The line is currently empty."
   else
-    katz_deli.each {|value, index| puts "Currently serving #{index+1}. "
+    katz_deli.each {|value, index| puts "Currently serving #{index+1}. #{value}."
   end
 
 def take_a_number (katz_deli, name) 
