41dfde85e2ef9bd209fabf86fedf5860c9e5e4c3
katy@karstaway.com <katy@karstaway.com>
2018-08-25 15:20:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,15 @@
 katz_deli = [];
 
 def line (katz_deli)
+  arr = []
   if katz_deli.length ==0
     puts "The line is currently empty."
   else
+    
     katz_deli.each_with_index do |item, index|
  
-    puts "The line is currently: #{item}. #{index +1}."
+    arr.push "#{item}. #{index +1}."
+    puts "The line is currently #{arr}"
   end
 end
 
