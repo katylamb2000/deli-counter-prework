c336bfceda187070979aa979b03eeb6394255b2b
katy@karstaway.com <katy@karstaway.com>
2018-08-25 14:55:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,9 @@ katz_deli = [];
 
 def line (katz_deli)
   if katz_deli.length ==0
-    puts "The line is empty"
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{katz_deli.length +1}. "
   end
 
 def take_a_number (katz_deli, name) 
